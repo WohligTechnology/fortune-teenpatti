c3682b16a77a705b9208b415ff216f2c73a5eee7
maheshnadar <maheshnadar@wohlig.com>
2018-02-13 18:50:24 +0530

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -25,49 +25,18 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+  //   .state('app', {
+  //   url: '/app',
+  //   templateUrl: 'templates/app.html',
+  //   controller: 'AppCtrl'
+  // })
+  .state('lobby', {
+    url: '/lobby',
+    templateUrl: 'templates/lobby.html',
+    controller: 'LobbyCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  ;
+  // if none of the above states are matche d, use this as the fallback
+  $urlRouterProvider.otherwise('lobby');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+myApp=angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
