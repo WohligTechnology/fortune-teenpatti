40ec6494b1a5e79e07deb39700b168f58c1e45b2
maheshnadar <mahesh.nadar@wohlig.com>
2018-04-09 12:33:12 +0530

    chaal animation and tip  modal ui

--- www/js/directive.js
+++ www/js/directive.js
@@ -99,6 +99,19 @@ myApp.directive('card', function () {
       link: function ($scope, element, attr) {}
     };
   })
+  .directive('tipAmount', function () {
+    return {
+      restrict: 'E',
+      replace: false,
+      scope: {
+        amount: "=ngAmount",
+        PlayerNo: "=ngPlayerNo",
+        players: "=ngPlayer"
+      },
+      templateUrl: 'templates/directive/tip-amount.html',
+      link: function ($scope, element, attr) {}
+    };
+  })
   .directive('leftMenu', function () {
     return {
       restrict: 'E',
