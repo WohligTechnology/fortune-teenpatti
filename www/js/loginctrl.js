cf09aea5c997bb5a81da36e4e692b6c31a860383
maheshnadar <mahesh.nadar@wohlig.com>
2018-04-10 15:22:38 +0530

    fix

--- www/js/loginctrl.js
+++ www/js/loginctrl.js
@@ -1,7 +1,8 @@
 myApp.controller("LoginCtrl", function ($scope, Service, $state, $ionicPlatform, $ionicModal, $timeout) {
   $ionicPlatform.ready(function () {
-    screen.orientation.lock('portrait')
+    screen.orientation.lock('portrait');
   })
+  screen.orientation.lock('portrait');
   $ionicModal.fromTemplateUrl('templates/modal/message.html', {
     scope: $scope,
     animation: 'slide-in-up'
